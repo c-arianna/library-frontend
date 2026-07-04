58b4bfa36f2daaeaf8352d5206f554055e9d5266
arianna <arianna.comi03@gmail.com>
2026-07-04 15:30:33 +0200

    feat(notifications): integrate websocket notification client

---
+++ src/app/core/services/notification.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { Client } from '@stomp/stompjs';
+import { Observable, Subject } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+
+@Injectable({ providedIn: 'root' })
+export class NotificationService {
+
+  private client!: Client;
+  private subject = new Subject<any>();
+ 
+  constructor(private auth: AuthService) {
+   
+    this.client = new Client({
+
+      webSocketFactory: () => new WebSocket('ws://localhost:4200/ws/notifications'),
+
+      connectHeaders: {
+        Authorization: `Bearer ${this.auth.getToken()}`
+      },
+
+      reconnectDelay: 0,
+
+      onConnect: () => {
+        console.log('WebSocket connected');
+
+        this.client.subscribe('/topic/books', message => {
+          const event = JSON.parse(message.body);
+          this.subject.next(event);
+        });
+      },
+
+      onDisconnect: () => {
+        console.log('WebSocket disconnetted');
+      },
+
+      onStompError: frame => {
+        console.error('STOMP error:', frame.headers['message']);
+        console.error('STOMP details:', frame.body);
+      },
+
+      onWebSocketError: error => {
+        console.error('WebSocket error', error);
+      },
+
+      onWebSocketClose: (evt) => {
+        console.warn('WebSocket closed',
+          {
+            code: evt.code,
+            reason: evt.reason
+          }
+        );
+      }
+    });
+
+    this.client.activate();
+  }
+
+  messages(): Observable<any> {
+    return this.subject.asObservable();
+  }
+
+  disconnect(): void {
+    if (this.client?.active) {
+      this.client.deactivate();
+    }
+  }
+
+}
